d04ff7e015d52841d41aa4d0c7887a6554a45970
Murtaza <murtazafhathiyari@gmail.com>
2025-09-04 09:58:02 -0400

    added a handshake module to the arduino code

--- Software/arduino.py
+++ Software/arduino.py
@@ -6,18 +6,25 @@ class ArduinoCommunication:
 
     def __init__(self,
                  port='/dev/ttyACM0',
-                 baud_rate=9600):
+                 baud_rate=9600,
+                 handshake_interval=10):
         
         self.port = port
         self.baud_rate = baud_rate
+        self.handshake_interval = handshake_interval  # seconds
 
         self.ser = serial.Serial(port, baud_rate)
+        time.sleep(2)  # wait for the serial connection to initialize
         print("Arduino Initialized Successfully!")
 
 
     def send_trigger(self):
             self.ser.write(b'T')  # send trigger command
-            trigger_time = time.time()
-            print("Trigger sent.")
+            trigger_time = time.perf_counter_ns()  # Timestamp in nanoseconds
+            print("[Arduino] Trigger sent.")
 
-            return trigger_time
+            return trigger_time 
+    
+    def handshake(self):
+            self.ser.write(b'H')  # send handshake command
+            print("[Arduino] Handshake sent.")
--- Software/hardware_trigger/hardware_trigger.ino
+++ Software/hardware_trigger/hardware_trigger.ino
@@ -1,23 +1,35 @@
-// Arduino: Serial Trigger Receiver
+// Arduino: Serial Trigger + Handshake Receiver
 // Listens for serial input and generates a TTL pulse on pin 8
+// - 'T' → generate a TTL pulse on pin 8 and reply "RCVD"
+// - 'H' → reply "HANDSHAKE_OK"
 
-const int triggerPin = 8;   // Pin used for TTL output
-const int pulseDuration = 10; // pulse width in ms
+const int triggerPin = 8;        // Pin used for TTL output
+const int handshakePin = 7;
+const int pulseDuration = 10;    // pulse width in ms
 
 void setup() {
   pinMode(triggerPin, OUTPUT);
+  pinMode(handshakePin,OUTPUT);
   digitalWrite(triggerPin, LOW);
+  digitalWrite(handshakePin, LOW);
   Serial.begin(9600); // Match Python baud rate
 }
 
 void loop() {
   if (Serial.available() > 0) {
     char cmd = Serial.read();
-    if (cmd == 'T') { // trigger command
+
+    if (cmd == 'T') {  // Trigger command
       Serial.print("RCVD");
       digitalWrite(triggerPin, HIGH);
       delay(pulseDuration);
       digitalWrite(triggerPin, LOW);
+    } 
+    else if (cmd == 'H') {  // Handshake command
+      Serial.print("HANDSHAKE_OK");
+      digitalWrite(handshakePin, HIGH);
+      delay(pulseDuration);
+      digitalWrite(handshakePin, LOW);
     }
   }
 }
